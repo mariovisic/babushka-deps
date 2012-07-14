23ba18f0b1aed9820e7c8449a7327d6596558c5b
Mario Visic <mario@mariovisic.com>
2012-07-14 21:51:42 +1000

    Added some usage comments to the top of provisioning.

--- desktoppr/provision.rb
+++ desktoppr/provision.rb
@@ -1,3 +1,16 @@
+# Usage:
+#
+# Provisioning a machine can either be the web frontend, the redis backend or
+# the database. You can also provision all three at once.
+#
+# NOTE: Please add your public key as a param when running your command.
+# DO NOT enter it during the prompt, this seems to cause issues.
+#
+# babushka provision web      public_key='key-goes-here'
+# babushka provision database public_key='key-goes-here'
+# babushka provision worker   public_key='key-goes-here'
+# babushka provision complete public_key='key-goes-here'
+
 module RemoteHelpers
   def as user, &block
     previous_user, @user = @user, user
@@ -40,27 +53,24 @@ end
 
 include RemoteHelpers
 
-# General setup for a machine
-dep 'provision base', :host, :public_key, :app_user  do
-  public_key.default!((dependency.load_path.parent / 'config/authorized_public_key').read)
-
-  requires 'conversation:public key in place'.with(host, public_key)
-  requires 'conversation:babushka bootstrapped'.with(host)
-
-  meet {
-    as('root') {
-      remote_babushka 'benhoskings:set.locale', :locale_name => 'en_AU'
-      remote_babushka '"benhoskings:libssl headers.managed"'
-      remote_babushka 'benhoskings:utc'
-      remote_babushka '"benhoskings:lamp stack removed"'
-      remote_babushka '"benhoskings:postfix removed"'
-      remote_babushka '"benhoskings:user setup for provisioning"', :username => app_user, :key => public_key
-      remote_babushka 'benhoskings:system'
-    }
+dep 'system setup', :host, :public_key, :app_user do
+  as('root') {
+    remote_babushka 'benhoskings:set.locale', :locale_name => 'en_AU'
+    remote_babushka '"benhoskings:libssl headers.managed"'
+    remote_babushka 'benhoskings:utc'
+    remote_babushka '"benhoskings:lamp stack removed"'
+    remote_babushka '"benhoskings:postfix removed"'
+    remote_babushka '"benhoskings:user setup for provisioning"', :username => app_user, :key => public_key
+    remote_babushka 'benhoskings:system'
   }
 end
 
-# General rails app setup
+dep 'provision base', :host, :public_key, :app_user  do
+  requires 'conversation:public key in place'.with(host, public_key)
+  requires 'conversation:babushka bootstrapped'.with(host)
+  requires 'system setup'.with(host, public_key, app_user)
+end
+
 dep 'provision app', :host, :public_key, :app_user do
   as(app_user) {
     remote_babushka 'mariovisic:node.src'
@@ -68,10 +78,28 @@ dep 'provision app', :host, :public_key, :app_user do
   }
 end
 
-# Frontend rails server setup
-dep 'provision web', :host, :env, :public_key, :app_user do
+dep 'provision web', :host, :public_key, :app_user do
   requires [
     'provision base'.with(host, public_key, app_user),
     'provision app'.with(host, public_key, app_user)
+  ]
+end
+
+dep 'provision worker', :host, :public_key, :app_user do
+  requires [
+    'provision base'.with(host, public_key, app_user),
+    'provision app'.with(host, public_key, app_user)
+  ]
+end
+
+dep 'provision database', :host, :public_key, :app_user do
+  requires [ 'provision base'.with(host, public_key, app_user) ]
+end
+
+dep 'provision complete', :host, :public_key, :app_user do
+  requires [
+    'provision web'.with(host, public_key, app_user),
+    'provision database'.with(host, public_key, app_user),
+    'provision worker'.with(host, public_key, app_user)
   ]
 end
